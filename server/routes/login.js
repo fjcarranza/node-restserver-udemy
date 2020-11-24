b078ddf0ac3d1ad2e1c58c34777e51bd954896cf
Fernando <FJCarranza@hotmail.com>
2020-11-24 00:47:37 -0300

    Anda login

--- server/routes/login.js
+++ server/routes/login.js
@@ -1,12 +1,42 @@
 const express = require('express');
 const bcrypt = require('bcrypt');
 const Usuario = require('../models/usuario');
+const usuario = require('../models/usuario');
 const app = express();
 
 app.post('/login', (req, res) => {
-    res.json({
-        ok: true
+    let body=req.body;
+
+    Usuario.findOne({email: body.email}, (err, usuarioDB)=>{
+        if(err){
+            return res.status(500).json({
+                ok:false,
+                err
+            });
+        }
+        if(!usuarioDB){
+            return res.status(400).json({
+                ok:false,
+                err:{
+                    message:"Usuaior o Clave incorrectas (usuario)"
+                }
+            });
+        }
+        if(!bcrypt.compareSync(body.password, usuarioDB.password)){
+            return res.status(400).json({
+                ok:false,
+                err:{
+                    message:"Usuaior o Clave incorrectas (clave)"
+                }
+            });
+        }
+        res.json({
+            ok: true,
+            usuario: usuarioDB,
+            token: '123'
+        });
     });
+    
 });
 
 
--- server/server.js
+++ server/server.js
@@ -12,16 +12,11 @@ app.use(bodyParser.json())
 // configuracion global de rutas es el archivo index donde estan las rutas
 app.use(require('./routes/index'));
 
-
-
-
-
-mongoose.connect(process.env.URLDB, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true },
-    (err, res) => {
+mongoose.connect(process.env.URLDB, (err, res) => {
         if (err) throw err;
         console.log("Base de Datos online");
-    });
+});
 
 app.listen(process.env.PORT, () => {
-    console.log("Escuchando Puerto 3000 ");
-})
+    console.log("Escuchando Puerto: ", process.env.PORT);
+});
